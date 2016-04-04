a635d5fd1ad98643709cf36f2a146ebc0841da60
AmitaPatel3 <amitapatel3@hotmail.com>
2016-04-04 17:01:09 -0600

    created post route to twilio

--- server/app.js
+++ server/app.js
@@ -3,10 +3,20 @@
 var express = require('express');
 var app = new express.Router();
 
-app.get('/sms', function getCb(req, res) {
-  console.log('REQUEST to/sms');
+app.post('/sms', function getCb(req, res) {
+  
+  console.log(req.body);
 
-  res.send({ ok: true });
+  var twiml = [
+    '<?xml version="1.0" encoding="UTF-8" ?>',
+    '<Response>',
+    '  <Message>You said: ' + req.body.Body + '</Message>',
+    '</Response>'
+  ].join('');
+
+  res.set('Content-Type', 'text/xml');
+  res.send(twiml);
+
 });
 
 module.exports = app;
