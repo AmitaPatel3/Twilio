6a5a569d772ab84ce25a68633d19cc6d996008f1
AmitaPatel3 <amitapatel3@hotmail.com>
2016-04-04 16:35:47 -0600

    added body parser and changed GET route to sms

---
+++ server/app.js
@@ -0,0 +1,12 @@
+
+/* eslint-disable no-console */
+var express = require('express');
+var app = new express.Router();
+
+app.get('/sms', function getCb(req, res) {
+  console.log('REQUEST to/sms');
+
+  res.send({ ok: true });
+});
+
+module.exports = app;
